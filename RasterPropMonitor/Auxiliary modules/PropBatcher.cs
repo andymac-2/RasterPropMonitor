fe1612190dad2a8c723174d7ec1ef4a254a4dfae
JonnyOThan <jonnyothan@gmail.com>
2023-05-06 10:49:10 -0400

    prop batching can now be based on model path

--- RasterPropMonitor/Auxiliary modules/PropBatcher.cs
+++ RasterPropMonitor/Auxiliary modules/PropBatcher.cs
@@ -14,24 +14,37 @@ namespace JSI.Auxiliary_modules
         {
             this.batchID = batchID;
             propName = node.GetValue(nameof(propName));
+            modelPath = node.GetValue(nameof(modelPath));
             transformName = node.GetValue(nameof(transformName));
             node.TryGetValue(nameof(keepProp), ref keepProp);
         }
 
         public readonly int batchID;
         public readonly string propName;
+        public readonly string modelPath;
         public readonly string transformName;
         public readonly bool keepProp = true;
-        
     }
 
     public class PropBatcher : InternalModule
     {
-        static Dictionary<string, BatchFilter> x_propNameToFilter;
+        static Dictionary<string, BatchFilter> x_propNameToFilter = new Dictionary<string, BatchFilter>();
+        static Dictionary<string, BatchFilter> x_modelPathToFilter = new Dictionary<string, BatchFilter>();
         
+        static bool TryAdd(Dictionary<string, BatchFilter> dict, string key, BatchFilter value)
+        {
+            if (dict.ContainsKey(key))
+            {
+                return false;
+            }
+            dict.Add(key, value);
+            return true;
+        }
+
         public static void ModuleManagerPostLoad()
         {
-            x_propNameToFilter = new Dictionary<string, BatchFilter>();
+            x_propNameToFilter.Clear();
+            x_modelPathToFilter.Clear();
 
             var batchNodes = GameDatabase.Instance.GetConfigNodes("PROP_BATCH");
             int nextBatchID = 0;
@@ -41,16 +54,14 @@ namespace JSI.Auxiliary_modules
                 {
                     var batchFilter = new BatchFilter(filterNode, nextBatchID);
                     
-                    if (batchFilter.propName != null)
+                    if (batchFilter.propName != null && !TryAdd(x_propNameToFilter, batchFilter.propName, batchFilter))
                     {
-                        if (x_propNameToFilter.ContainsKey(batchFilter.propName))
-                        {
-                            JUtil.LogErrorMessage(null, "PROP_BATCH: Tried to add prop {0} to multiple filters", batchFilter.propName);
-                        }
-                        else
-                        {
-                            x_propNameToFilter.Add(batchFilter.propName, batchFilter);
-                        }
+                        JUtil.LogErrorMessage(null, "PROP_BATCH: Tried to add prop {0} to multiple batches", batchFilter.propName);
+                    }
+
+                    if (batchFilter.modelPath != null && !TryAdd(x_modelPathToFilter, batchFilter.modelPath + "(Clone)", batchFilter))
+                    {
+                        JUtil.LogErrorMessage(null, "PROP_BATCH: Tried to add model {0} to multipler batches", batchFilter.modelPath);
                     }
                 }
 
@@ -62,10 +73,19 @@ namespace JSI.Auxiliary_modules
         {
             if (!prop.hasModel) return null;
 
-            // currently, we only support matching by propName but that will change eventually
             if (x_propNameToFilter.TryGetValue(prop.propName, out var batchFilter))
             {
                 return batchFilter;
+            }
+
+            var modelRoot = prop.transform.Find("model");
+            for (int childIndex = 0; childIndex < modelRoot.childCount; ++childIndex)
+            {
+                var model = modelRoot.GetChild(childIndex);
+                if (x_modelPathToFilter.TryGetValue(model.name, out batchFilter))
+                {
+                    return batchFilter;
+                }
             }
 
             return null;
@@ -123,13 +143,15 @@ namespace JSI.Auxiliary_modules
 
             if (batchRoots.Count > 0)
             {
+                int batchedTransforms = 0;
                 foreach (var batchRoot in batchRoots.Values)
                 {
+                    batchedTransforms += batchRoot.childCount;
                     StaticBatchingUtility.Combine(batchRoot.gameObject);
                     JUtil.LogMessage(null, "PROP_BATCH: batching {0} transforms under {1}", batchRoot.childCount, batchRoot.parent.name);
                 }
 
-                JUtil.LogMessage(null, "PROP_BATCH: old prop count: {0}; new prop count: {1}; delta {2}", internalModel.props.Count, newProps.Count, internalModel.props.Count - newProps.Count);
+                JUtil.LogMessage(null, "PROP_BATCH: old prop count: {0}; new prop count: {1}; delta {2}; total batched transforms {3}", internalModel.props.Count, newProps.Count, internalModel.props.Count - newProps.Count, batchedTransforms);
                 internalModel.props = newProps;
             }
         }
