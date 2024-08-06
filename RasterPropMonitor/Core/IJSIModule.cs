a158ba26c765482b87909810df9cd7042f97621b
JonnyOThan <jonnyothan@gmail.com>
2024-08-06 09:25:20 -0400

    refactor: extract DynamicMethodDelegateFactory to its own file -someday IJSIModule should become a better interface for 3rd party additions, but that day is probably not today

---
+++ RasterPropMonitor/Core/IJSIModule.cs
@@ -0,0 +1,33 @@
+﻿/*****************************************************************************
+ * RasterPropMonitor
+ * =================
+ * Plugin for Kerbal Space Program
+ *
+ *  by Mihara (Eugene Medvedev), MOARdV, and other contributors
+ * 
+ * RasterPropMonitor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, revision
+ * date 29 June 2007, or (at your option) any later version.
+ * 
+ * RasterPropMonitor is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with RasterPropMonitor.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
+using System;
+
+namespace JSI
+{
+    /// <summary>
+    /// This class exists to provide a base class that RasterPropMonitorComputer
+    /// manages for tracking various built-in plugin action handlers.
+    /// </summary>
+    public class IJSIModule
+    {
+        public Vessel vessel;
+    }
+}
