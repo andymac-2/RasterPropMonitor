9243d0a12cfaf6b8070f872d13af82d7af3bc1d7
JonnyOThan <jonnyothan@gmail.com>
2025-09-04 13:11:55 -0400

    switch KSPBT to nuget

--- RasterPropMonitor/Properties/AssemblyInfo.cs
+++ RasterPropMonitor/Properties/AssemblyInfo.cs
@@ -1,10 +1,11 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
+
+[assembly: AssemblyCopyright ("Copyright ©2013-2018 by Mihara, MOARdV, and other contributors, released under the terms of GNU GPLv3")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
-[assembly: AssemblyTitle ("RasterPropMonitor")]
-[assembly: AssemblyDescription ("RasterPropMonitor plugin for Kerbal Space Program")]
+[assembly: AssemblyDescription("RasterPropMonitor plugin for Kerbal Space Program")]
 
-// This supposedly helps avoid plugin depencency problems.
-[assembly: KSPAssembly("RasterPropMonitor", 0, 18)]
